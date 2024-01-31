2e5146e3c98ed7a5018d7b9b8557fa170c68d31a
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-01-31 13:03:41 +0330

    login stracture added

--- account_app/views.py
+++ account_app/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
-
-def login(request):
-    return render(request,'account_app/login.html')
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import User
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user= authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('/login/login')
+    else:
+        return render(request,'account_app/login.html')
